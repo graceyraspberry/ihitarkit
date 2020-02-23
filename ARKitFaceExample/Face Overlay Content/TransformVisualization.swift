31c029e5860dc47624c70f8ea349e44301412430
Grace Lam <gracelam@berkeley.edu>
2020-02-22 16:03:55 -0800

    example code with some prints of face pose and gaze

---
+++ ARKitFaceExample/Face Overlay Content/TransformVisualization.swift
@@ -0,0 +1,98 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Displays coordinate axes visualizing the tracked face pose (and eyes in iOS 12).
+*/
+
+import ARKit
+import SceneKit
+
+class TransformVisualization: NSObject, VirtualContentController {
+    
+    var contentNode: SCNNode?
+    
+    // Load multiple copies of the axis origin visualization for the transforms this class visualizes.
+    lazy var rightEyeNode = SCNReferenceNode(named: "coordinateOrigin")
+    lazy var leftEyeNode = SCNReferenceNode(named: "coordinateOrigin")
+    
+    var leftEyeArray: Array<Float> = Array()
+    var rightEyeArray: Array<Float> = Array()
+    var lookAtArray: Array<Float> = Array()
+    var timeArray: Array<Double> = Array()
+    var faceArray: Array<Float> = Array()
+    var count = 0
+    
+    /// - Tag: ARNodeTracking
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        // This class adds AR content only for face anchors.
+        guard anchor is ARFaceAnchor else { return nil }
+        
+        // Load an asset from the app bundle to provide visual content for the anchor.
+        contentNode = SCNReferenceNode(named: "coordinateOrigin")
+        
+        // Add content for eye tracking in iOS 12.
+        self.addEyeTransformNodes()
+        
+        // Provide the node to ARKit for keeping in sync with the face anchor.
+        return contentNode
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard #available(iOS 12.0, *), let faceAnchor = anchor as? ARFaceAnchor
+            else { return }
+        
+        rightEyeNode.simdTransform = faceAnchor.rightEyeTransform
+        leftEyeNode.simdTransform = faceAnchor.leftEyeTransform
+
+        var leftEyePosition = SCNVector3(leftEyeNode.simdTransform.columns.3.x, leftEyeNode.simdTransform.columns.3.y, leftEyeNode.simdTransform.columns.3.z)
+        var rightEyePosition = SCNVector3(rightEyeNode.simdTransform.columns.3.x, rightEyeNode.simdTransform.columns.3.y, rightEyeNode.simdTransform.columns.3.z)
+        var leftEyeX = leftEyeNode.simdTransform.columns.3.x
+        var rightEyeX = rightEyeNode.simdTransform.columns.3.x
+        let d = distance(float3(leftEyePosition), float3(rightEyePosition))
+//        print("\nInter-eye distance in centimeters: ", d * 100)
+//        print("\nLeft eye x: ", leftEyeX * 100)
+//        print("\nRight eye x: ", rightEyeX * 100)
+//        print(faceAnchor.lookAtPoint.x)
+        let x = faceAnchor.lookAtPoint.x
+        let timestamp = Date().timeIntervalSince1970
+        let facecoordinate = faceAnchor.transform.columns.3.x
+        leftEyeArray.append(leftEyeX)
+        rightEyeArray.append(rightEyeX)
+        lookAtArray.append(x)
+        timeArray.append(timestamp)
+        faceArray.append(facecoordinate)
+        
+        count = count + 1
+        if (count == 500) {
+            //write to csv file
+            print("\nLeft eye array")
+            print(leftEyeArray)
+            print("\nRight eye array")
+            print(rightEyeArray)
+            print("\nLook at point array")
+            print(lookAtArray)
+            print("\nTime array")
+            print(timeArray)
+            print("\nFace array")
+            print(faceArray)
+        }
+//        NSMutableData * data = [NSMutableData dataWithCapacity:0];
+//
+//        [data appendBytes:&leftEyeX length:sizeof(float)];
+//        data.write("/Users/cchen/Desktop/lefteye.csv", leftEyeX)
+//        NSData.write("/Users/cchen/Desktop/righteye.csv", rightEyeX)
+    }
+    
+    func addEyeTransformNodes() {
+        guard #available(iOS 12.0, *), let anchorNode = contentNode else { return }
+        
+        // Scale down the coordinate axis visualizations for eyes.
+        rightEyeNode.simdPivot = float4x4(diagonal: float4(3, 3, 3, 1))
+        leftEyeNode.simdPivot = float4x4(diagonal: float4(3, 3, 3, 1))
+        
+        anchorNode.addChildNode(rightEyeNode)
+        anchorNode.addChildNode(leftEyeNode)
+    }
+
+}
---
+++ ARKitFaceExample/ViewController.swift
@@ -0,0 +1,148 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Main view controller for the AR experience.
+*/
+
+import ARKit
+import SceneKit
+import UIKit
+
+class ViewController: UIViewController, ARSessionDelegate {
+    
+    // MARK: Outlets
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var tabBar: UITabBar!
+
+    // MARK: Properties
+
+    var contentControllers: [VirtualContentType: VirtualContentController] = [:]
+    
+    var selectedVirtualContent: VirtualContentType! {
+        didSet {
+            guard oldValue != nil, oldValue != selectedVirtualContent
+                else { return }
+            
+            // Remove existing content when switching types.
+            contentControllers[oldValue]?.contentNode?.removeFromParentNode()
+            
+            // If there's an anchor already (switching content), get the content controller to place initial content.
+            // Otherwise, the content controller will place it in `renderer(_:didAdd:for:)`.
+            if let anchor = currentFaceAnchor, let node = sceneView.node(for: anchor),
+                let newContent = selectedContentController.renderer(sceneView, nodeFor: anchor) {
+                node.addChildNode(newContent)
+            }
+        }
+    }
+    var selectedContentController: VirtualContentController {
+        if let controller = contentControllers[selectedVirtualContent] {
+            return controller
+        } else {
+            let controller = selectedVirtualContent.makeController()
+            contentControllers[selectedVirtualContent] = controller
+            return controller
+        }
+    }
+    
+    var currentFaceAnchor: ARFaceAnchor?
+    
+    // MARK: - View Controller Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+        sceneView.automaticallyUpdatesLighting = true
+        
+        // Set the initial face content.
+        tabBar.selectedItem = tabBar.items!.first!
+        selectedVirtualContent = VirtualContentType(rawValue: tabBar.selectedItem!.tag)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // AR experiences typically involve moving the device without
+        // touch input for some time, so prevent auto screen dimming.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // "Reset" to run the AR session for the first time.
+        resetTracking()
+    }
+
+    // MARK: - ARSessionDelegate
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        guard error is ARError else { return }
+        
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        
+        DispatchQueue.main.async {
+            self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
+        }
+    }
+    
+    /// - Tag: ARFaceTrackingSetup
+    func resetTracking() {
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    // MARK: - Error handling
+    
+    func displayErrorMessage(title: String, message: String) {
+        // Present an alert informing about the error that has occurred.
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+            alertController.dismiss(animated: true, completion: nil)
+            self.resetTracking()
+        }
+        alertController.addAction(restartAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: UITabBarDelegate {
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let contentType = VirtualContentType(rawValue: item.tag)
+            else { fatalError("unexpected virtual content tag") }
+        selectedVirtualContent = contentType
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+        
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        currentFaceAnchor = faceAnchor
+        
+        // If this is the first time with this anchor, get the controller to create content.
+        // Otherwise (switching content), will change content when setting `selectedVirtualContent`.
+        if node.childNodes.isEmpty, let contentNode = selectedContentController.renderer(renderer, nodeFor: faceAnchor) {
+            node.addChildNode(contentNode)
+        }
+    }
+    
+    /// - Tag: ARFaceGeometryUpdate
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        guard anchor == currentFaceAnchor,
+            let contentNode = selectedContentController.contentNode,
+            contentNode.parent == node
+            else { return }
+        
+        selectedContentController.renderer(renderer, didUpdate: contentNode, for: anchor)
+    }
+}
+
